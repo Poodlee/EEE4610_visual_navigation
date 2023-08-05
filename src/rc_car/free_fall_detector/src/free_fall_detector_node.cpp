86e1b7a0c9f3aee3d01dd0499faf146d7cc7cb26
Seongmin Jung <sung.minner@gmail.com>
2023-08-05 12:33:04 +0900

    Publish in callback function

--- src/rc_car/free_fall_detector/src/free_fall_detector_node.cpp
+++ src/rc_car/free_fall_detector/src/free_fall_detector_node.cpp
@@ -1,17 +1,28 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Imu.h"
 
-void chatterCallback(const sensor_msgs::Imu::ConstPtr& msg) {
-  ROS_INFO("Imu Seq: [%d]", msg->header.seq);
-  ROS_INFO("Imu Orientation x: [%f], y: [%f], z: [%f], w: [%f]",
-           msg->orientation.x, msg->orientation.y, msg->orientation.z,
-           msg->orientation.w);
-}
+class FreeFallDetector {
+ public:
+  FreeFallDetector() {
+    // Topic you want to publish
+    pub_ = n_.advertise<sensor_msgs::Imu>("/imu_pub", 1000);
+
+    // Topic you want to subscribe
+    sub_ = n_.subscribe("/imu/data", 1000, &FreeFallDetector::callback, this);
+  }
+
+  void callback(const sensor_msgs::Imu::ConstPtr& msg) { pub_.publish(msg); }
+
+ private:
+  ros::NodeHandle n_;
+  ros::Publisher pub_;
+  ros::Subscriber sub_;
+
+};  // End of class FreeFallDetector
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "free_fall_detector");
-  ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("/imu/data", 1000, chatterCallback);
+  FreeFallDetector free_fall_detector;
 
   ros::spin();
 
