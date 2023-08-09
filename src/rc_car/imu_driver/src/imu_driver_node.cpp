9473a9aa7c9ed8509ded9aff35ff7608c76c58a7
Seongmin Jung <sung.minner@gmail.com>
2023-08-10 01:35:47 +0900

    Lint

--- src/rc_car/imu_driver/src/imu_driver_node.cpp
+++ src/rc_car/imu_driver/src/imu_driver_node.cpp
@@ -1,14 +1,14 @@
-#include "imu_driver/imu_uart.hpp"
-
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
+
+#include "imu_driver/imu_uart.hpp"
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "imu_driver");
   ros::NodeHandle n;
   ros::Publisher imu_pub = n.advertise<sensor_msgs::Imu>("/imu/data", 1000);
 
-  while(ros::ok()){
+  while (ros::ok()) {
     sensor_msgs::Imu imu_data = read_result();
     imu_pub.publish(imu_data);
 
